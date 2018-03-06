66e9eed047ab6ed9c324702916d87fec9537bd8f
Isaac Lenton <isaac@isuniversal.com>
2018-03-06 11:24:10 +1000

    Moved functions to +ott and +ott/+utils packages
    Changed the readme to reflect the changes/how the toolbox is used.
    Added imports to ``most'' files so everything should work.

---
+++ +ott/+utils/iseven.m
@@ -0,0 +1,10 @@
+function x = iseven(input)
+% ISEVEN determines if an integer is even
+% Outputs a matrix of the same size as input with 1
+% for even and 0 for odd entries.
+%
+% Warning: Plays up if the the integer is of the order 10^16
+%
+% PACKAGE_INFO
+
+x = logical(mod(mod(input,2)+1,2));
---
+++ +ott/+utils/isodd.m
@@ -0,0 +1,10 @@
+function x = isodd(input)
+% ISODD determines if an integer is odd
+% Outputs a matrix the same size as input with
+% 1 for odd and 0 for even entries.
+%
+% Warning: Plays up if the the integer is of the order 10^16
+%
+% PACKAGE_INFO
+
+x = logical(mod(input,2));
